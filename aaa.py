413f5f5acf03c9024d0a53938ff2ef487721b023
aaalll1 <mlykm1847@gmail.com>
2024-09-06 19:31:22 +0300

    Update aaa.py

--- aaa.py
+++ aaa.py
@@ -7,13 +7,7 @@ bot = telebot.TeleBot('7249491605:AAFHyPh2nZHOhEjTDTCKG81yNhoJJwDnBhU')
 @bot.message_handler(commands=['start'])
 def start(message):
     # Send a welcome message
-    bot.send_message(message.chat.id, 'مرحباً بك! اكتب "السلام عليكم" لأحييك.')
-
-# Add a handler for messages that contain the phrase "السلام عليكم"
-@bot.message_handler(func=lambda message: 'السلام عليكم' in message.text)
-def greet(message):
-    # Respond with "وعليكم السلام تاج راسي علوش"
-    bot.send_message(message.chat.id, "وعليكم السلام تاج راسي علوش")
+    bot.send_message(message.chat.id, 'مرحبا!')
 
 # Start polling Telegram servers
 bot.polling()
